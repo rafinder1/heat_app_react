28392665200e83fa7843793695c4f039951d0f20
rafinder1 <rafpro33@gmail.com>
2023-09-07 21:33:46 +0200

    fetch type layers

--- src/screens/MultivariateAnalysis.js
+++ src/screens/MultivariateAnalysis.js
@@ -1,5 +1,101 @@
+import { useState, useEffect } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+
+import CardHeader from '../components/CardHeader';
+import ClimateZoneDropdown from '../components/ClimateZoneDropdown';
+import InputField from '../components/InputField';
+
+import { options } from '../CalculateTemperature/constans/Constans';
+
 function MultiAnalysis() {
-    return (<h1>Multivariate Analysis</h1>)
+    const [selectedTemp, setSelectedZone] = useState(null);
+    const [inputTemp, setInputTemp] = useState();
+    const [inputPower, setInputPower] = useState();
+
+
+    const handleDropdownSelect = (eventKey) => {
+
+        const selectedTemperature = parseInt(eventKey.split(': ')[1]);
+
+        setSelectedZone(selectedTemperature);
+    };
+
+    const handleInputTemp = (event) => {
+        const inputValue = event.target.value;
+
+        if (inputValue !== '') {
+            const parsedValue = parseFloat(inputValue);
+
+            if (!isNaN(parsedValue)) {
+                setInputTemp(parsedValue)
+            }
+        }
+    }
+
+    const handleInputPower = (event) => {
+        const inputValue = event.target.value;
+
+        if (inputValue !== '') {
+            const parsedValue = parseFloat(inputValue);
+
+            if (!isNaN(parsedValue)) {
+                setInputPower(parsedValue)
+            }
+        }
+    }
+
+
+    const useDataFetching = () => {
+        const [typeMaterial, setTypeMaterial] = useState([]);
+        const fetchData = async () => {
+
+            const response = await fetch('http://127.0.0.1:8000/api/type_layers', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const jsonData = await response.json();
+            setTypeMaterial(jsonData.type_layers);
+        }
+        useEffect(() => {
+            fetchData();
+        }, []);
+
+        return typeMaterial;
+    };
+
+    const typeMaterial = useDataFetching();
+
+    console.log(typeMaterial)
+    return (
+        <Card>
+            <CardHeader title="Basic Partition Data" />
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <ClimateZoneDropdown
+                            selectedTemp={selectedTemp}
+                            options={options}
+                            onSelect={handleDropdownSelect}
+                        />
+                        <InputField
+                            value={inputTemp}
+                            onChange={handleInputTemp}
+                            placeholder={'Temperature [°C]'}
+                        />
+                        <InputField
+                            value={inputPower}
+                            onChange={handleInputPower}
+                            placeholder={'Power Heater [W/m2]'}
+                        />
+                        <p>Choose Type Material</p>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+
+    )
 }
 
 export default MultiAnalysis
