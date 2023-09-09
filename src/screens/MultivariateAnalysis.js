0fb3f9837d68bc303fc5ffa31a3853275ac2b67c
rafinder1 <rafpro33@gmail.com>
2023-09-09 13:05:13 +0200

    add dropdown with type layers

--- src/screens/MultivariateAnalysis.js
+++ src/screens/MultivariateAnalysis.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Card, Col, Row, Dropdown } from 'react-bootstrap';
 
 import CardHeader from '../components/CardHeader';
 import ClimateZoneDropdown from '../components/ClimateZoneDropdown';
@@ -11,6 +11,7 @@ function MultiAnalysis() {
     const [selectedTemp, setSelectedZone] = useState(null);
     const [inputTemp, setInputTemp] = useState();
     const [inputPower, setInputPower] = useState();
+    const [selectedOption, setSelectedOption] = useState(null);
 
 
     const handleDropdownSelect = (eventKey) => {
@@ -67,6 +68,10 @@ function MultiAnalysis() {
 
     const typeMaterial = useDataFetching();
 
+    const onSelect = (option) => {
+        setSelectedOption(option);
+    };
+
     console.log(typeMaterial)
     return (
         <Card>
@@ -90,6 +95,28 @@ function MultiAnalysis() {
                             placeholder={'Power Heater [W/m2]'}
                         />
                         <p>Choose Type Material</p>
+                        <Dropdown onSelect={onSelect}>
+                            <Dropdown.Toggle variant="light" style={{ width: '100%' }}>
+                                {selectedOption !== null
+                                    ? `Selected: ${selectedOption.fields.type_layer}`
+                                    : 'Select Layer Type'}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu style={{ width: '100%' }}>
+                                {typeMaterial.map((item) => (
+                                    <Dropdown.Item
+                                        key={item.pk}
+                                        eventKey={item.fields.type_layer}
+                                        style={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        {item.fields.type_layer}
+                                    </Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Col>
                 </Row>
             </Card.Body>
