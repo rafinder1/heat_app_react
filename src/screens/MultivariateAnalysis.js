e16d970b331be54ea5a5812963bf69868a4c4ff2
rafinder1 <rafpro33@gmail.com>
2023-09-21 18:05:37 +0200

    create handler with mvc - calc

--- src/screens/MultivariateAnalysis.js
+++ src/screens/MultivariateAnalysis.js
@@ -18,6 +18,7 @@ function MultiAnalysis() {
     const [thickness, setThickness] = useState();
     const [selectThickness, setSelectThickness] = useState(null);
     const [rows, setRows] = useState([]);
+    const [mvc, setMVC] = useState(null);
 
 
     const handleDropdownSelect = (eventKey) => {
@@ -141,13 +142,15 @@ function MultiAnalysis() {
 
     }
     const onSelectThickness = (option) => {
-        setSelectThickness(option);
+        setSelectThickness(parseFloat(option));
     };
 
     const handleAddLayer = () => {
         if (selectedOption !== null && selectMaterial !== null && selectThickness !== null) {
+
+
             materials.material.forEach(element => {
-                if (element.fields.name_layer === selectMaterial) {
+                if (element.fields.name_layer === selectMaterial && element.fields.thickness === selectThickness) {
                     setRows([...rows, element.fields])
                 }
             })
@@ -156,6 +159,42 @@ function MultiAnalysis() {
 
     const handleDelAllRows = () => {
         setRows([])
+    }
+
+    const handleCalculate = async () => {
+        const requestData = {
+            data_building_partition: rows,
+            heat_information: {
+                inside_temperature: null,
+                outside_temperature: selectedTemp, // Use the selected temperature
+                inside_heater_power: inputPower,
+                outside_heater_power: null,
+            },
+            method: 'finite_element_method',
+        };
+        const response = await fetch('http://127.0.0.1:8000/api/multi_variant_calc', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        })
+        if (response.ok) {
+            const polystyrene_information = await response.json();
+            setMVC(polystyrene_information);
+
+
+        } else {
+            // Handle error response
+            const result = "Handle error response"
+            setMVC(result)
+        }
+
+    }
+
+
+    if (mvc !== null) {
+        console.log(mvc)
     }
 
 
@@ -170,11 +209,11 @@ function MultiAnalysis() {
                             options={options}
                             onSelect={handleDropdownSelect}
                         />
-                        <InputField
+                        {/* <InputField
                             value={inputTemp}
                             onChange={handleInputTemp}
                             placeholder={'Temperature [°C]'}
-                        />
+                        /> */}
                         <InputField
                             value={inputPower}
                             onChange={handleInputPower}
@@ -289,6 +328,32 @@ function MultiAnalysis() {
                                 <tr>
                                     <td colSpan={5}>Outer Wall</td>
                                 </tr>
+                            </tbody>
+                        </Table>
+
+                        <Button variant="success" onClick={handleCalculate} >Calculate</Button>
+
+                        <Table striped bordered hover variant="light">
+                            <thead>
+                                <tr>
+                                    <th>Name Layer</th>
+                                    <th>Thickness [m]</th>
+                                    <th>λ [W/mK]</th>
+                                    <th>Temperature [°C]</th>
+                                    <th>Cost [PLN]</th>
+                                    <th>Comments</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* {mvc.map((row, index) => (
+                                    <tr key={index}>
+                                        <td>{row.model}</td>
+                                        <td>{row.name_layer}</td>
+                                        <td>{row.thickness}</td>
+                                        <td>{row.thermal_conductivity}</td>
+                                        <td>{row.cost}</td>
+                                    </tr>
+                                ))} */}
                             </tbody>
                         </Table>
                     </Col>
